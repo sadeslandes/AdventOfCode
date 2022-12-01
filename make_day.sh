b18b99295f7e60e23b7d9b8d3848cf0ca60ae579
Samuel Deslandes <sadeslandes@gmail.com>
2022-12-01 00:38:45 -0500

    2022 setup

--- make_day.sh
+++ make_day.sh
@@ -19,11 +19,12 @@ mkdir -p "$DIR/aoc/aoc$year/day$day" &&
     (
         cp "$DIR/template.py" "$DIR/aoc/aoc$year/day$day" &&
         mv "$DIR/aoc/aoc$year/day$day/template.py" "$DIR/aoc/aoc$year/day$day/day$day.py" &&
-        sed -i -e "s/url_template/https:\/\/adventofcode.com\/$year\/day\/$day/g" "$DIR/aoc/aoc$year/day$day/day$day.py"
+        sed -i -e "s/url_template/https:\/\/adventofcode.com\/$year\/day\/$day/g" "$DIR/aoc/aoc$year/day$day/day$day.py" &&
+        touch "$DIR/aoc/aoc$year/day$day/__init__.py"
     )) &&
     mkdir -p "$DIR/tests/$year/$day" &&
     ([ -f "$DIR/tests/$year/$day/s0.txt" ] || echo -e "${year}_$day\n<sample here>\n-\n-" > "$DIR/tests/$year/$day/s0.txt") &&
     echo "https://adventofcode.com/$year/day/$day" &&
-    reset_wsl2_interop > /dev/null &&
-    powershell.exe -c Start-Process "https://adventofcode.com/$year/day/$day" > /dev/null &&
+    # reset_wsl2_interop > /dev/null &&
+    # powershell.exe -c Start-Process "https://adventofcode.com/$year/day/$day" > /dev/null &&
     curl "https://adventofcode.com/$year/day/$day/input" --cookie "session=$AOC_SESSION" -o "$DIR/aoc/aoc$year/day$day/input.txt" --silent
