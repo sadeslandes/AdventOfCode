e532b4d52143d084c7604327e501aa74c1966518
Samuel Deslandes <sadeslandes@gmail.com>
2022-12-02 00:53:29 -0500

    formatting

--- aoc/aoc2022/day2/day2.py
+++ aoc/aoc2022/day2/day2.py
@@ -2,46 +2,49 @@
 from os import path
 
 HAND_SCORES = {
-    'R': 1,
-    'P': 2,
-    'S': 3,
+    "R": 1,
+    "P": 2,
+    "S": 3,
 }
 
 OUTCOME_SCORES = {
-    'L': 0,
-    'D': 3,
-    'W': 6,
-    'X': 0,
-    'Y': 3,
-    'Z': 6,
+    "L": 0,
+    "D": 3,
+    "W": 6,
+    "X": 0,
+    "Y": 3,
+    "Z": 6,
 }
 
 HAND_MAP = {
-    'A': 'R',
-    'B': 'P',
-    'C': 'S',
-    'X': 'R',
-    'Y': 'P',
-    'Z': 'S',
+    "A": "R",
+    "B": "P",
+    "C": "S",
+    "X": "R",
+    "Y": "P",
+    "Z": "S",
 }
 
-HANDS = ('S', 'R', 'P')
+HANDS = ("S", "R", "P")
+
 
 def judge_hand(opponent, you):
     if opponent == you:
-        return 'D'
+        return "D"
     elif you == HANDS[(HAND_SCORES[opponent] + 1) % 3]:
-        return 'W'
+        return "W"
     else:
-        return 'L'
+        return "L"
+
 
 def derive_hand(opponent, outcome):
-    if outcome == 'Y': # draw
+    if outcome == "Y":  # draw
         return opponent
-    elif outcome == 'X': # lose
+    elif outcome == "X":  # lose
         return HANDS[(HAND_SCORES[opponent] - 1) % 3]
-    else: # win
+    else:  # win
         return HANDS[(HAND_SCORES[opponent] + 1) % 3]
+
 
 # Part 1
 def part1(inpt: str):
@@ -49,7 +52,7 @@ def part1(inpt: str):
     total = 0
     for line in inpt_lines:
         opponent, you = [HAND_MAP[x] for x in line.split()]
-        total += (OUTCOME_SCORES[judge_hand(opponent, you)] + HAND_SCORES[you])
+        total += OUTCOME_SCORES[judge_hand(opponent, you)] + HAND_SCORES[you]
     return total
 
 
@@ -59,8 +62,12 @@ def part2(inpt: str):
     total = 0
     for line in inpt_lines:
         opponent, outcome = line.split()
-        total += (OUTCOME_SCORES[outcome] + HAND_SCORES[derive_hand(HAND_MAP[opponent], outcome)])
+        total += (
+            OUTCOME_SCORES[outcome]
+            + HAND_SCORES[derive_hand(HAND_MAP[opponent], outcome)]
+        )
     return total
+
 
 if __name__ == "__main__":
     from aoc.utils import Timer
