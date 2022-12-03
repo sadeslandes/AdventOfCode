4813bc5dbfbe254a72fdecb0dc56f7d5e9e1b90e
Samuel Deslandes <sadeslandes@gmail.com>
2022-12-03 02:21:50 -0500

    day3

---
+++ aoc/aoc2022/day3/day3.py
@@ -0,0 +1,42 @@
+"""https://adventofcode.com/2022/day/3"""
+import string
+
+from os import path
+from operator import iand
+from functools import reduce
+
+PRIORITY = {c: i + 1 for i, c in enumerate(string.ascii_letters)}
+
+
+# Part 1
+def part1(inpt: str):
+    inpt_lines = inpt.splitlines()
+    total = 0
+    for line in inpt_lines:
+        mid = len(line) // 2
+        common_item_type = next(iter(set(line[:mid]) & set(line[mid:])))
+        total += PRIORITY[common_item_type]
+    return total
+
+
+# Part 2
+def part2(inpt: str):
+    inpt_lines = inpt.splitlines()
+    total = 0
+    for group in [inpt_lines[i : i + 3] for i in range(0, len(inpt_lines), 3)]:
+        common_item_type = next(iter(reduce(iand, [set(g) for g in group])))
+        total += PRIORITY[common_item_type]
+    return total
+
+
+if __name__ == "__main__":
+    from aoc.utils import Timer
+
+    with open(path.join(path.dirname(__file__), "input.txt")) as f:
+        data = f.read()
+    with Timer() as t1:
+        p1 = part1(data)
+    print(f"Part 1: {t1} {p1}")
+    with Timer() as t2:
+        p2 = part2(data)
+    print(f"Part 2: {t2} {p2}")
