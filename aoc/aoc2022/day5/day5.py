5f21521d5645e3505ea7c51e322b4c1299735e0b
Samuel Deslandes <sadeslandes@gmail.com>
2022-12-05 00:50:42 -0500

    day5

---
+++ aoc/aoc2022/day5/day5.py
@@ -0,0 +1,63 @@
+"""https://adventofcode.com/2022/day/5"""
+from inspect import stack
+from os import path
+from collections import defaultdict
+import re
+
+
+def parse_input(inpt: str):
+    crates, instructions = inpt.split("\n\n")
+    return parse_stacks(crates), parse_instructions(instructions)
+
+
+def parse_stacks(crates: str):
+    stacks = defaultdict(list)
+    idx_map = dict()
+    stack_count = 0
+    for line in reversed(crates.splitlines()):
+        for i, c in enumerate(line):
+            if c.isdecimal():
+                stack_count += 1
+                idx_map[i] = stack_count
+            elif c.isalpha():
+                stacks[idx_map[i]].append(c)
+    return stacks
+
+
+def parse_instructions(instructions: str):
+    pattern = re.compile(r"move (\d+) from (\d+) to (\d+)")
+    return [
+        tuple(int(n) for n in pattern.match(line).groups())
+        for line in instructions.splitlines()
+    ]
+
+
+# Part 1
+def part1(inpt: str):
+    stacks, instructions = parse_input(inpt)
+    for count, src, dst in instructions:
+        for _ in range(count):
+            stacks[dst].append(stacks[src].pop())
+    return "".join(stacks[i + 1][-1] for i in range(len(stacks)))
+
+
+# Part 2
+def part2(inpt: str):
+    stacks, instructions = parse_input(inpt)
+    for count, src, dst in instructions:
+        stacks[dst] += stacks[src][-1 * count :]
+        del stacks[src][-1 * count :]
+    return "".join(stacks[i + 1][-1] for i in range(len(stacks)))
+
+
+if __name__ == "__main__":
+    from aoc.utils import Timer
+
+    with open(path.join(path.dirname(__file__), "input.txt")) as f:
+        data = f.read()
+    with Timer() as t1:
+        p1 = part1(data)
+    print(f"Part 1: {t1} {p1}")
+    with Timer() as t2:
+        p2 = part2(data)
+    print(f"Part 2: {t2} {p2}")
