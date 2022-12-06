3b4116d1848152de8cf383802bbc6498b7e3b208
Samuel Deslandes <sadeslandes@gmail.com>
2022-12-06 10:38:39 -0500

    Update timer to measure ns (display is us)

--- aoc/utils.py
+++ aoc/utils.py
@@ -1,4 +1,4 @@
-from time import perf_counter
+from time import perf_counter_ns
 
 __all__ = ["Timer"]
 
@@ -10,9 +10,9 @@ class Timer:
 
     def timer(func):
         def wrapper(*args, **kwargs):
-            t1 = perf_counter()
+            t1 = perf_counter_ns()
             result = func(*args, **kwargs)
-            elapsed = perf_counter() - t1
+            elapsed = perf_counter_ns() - t1
             color = Timer.__red if elapsed > 10 else Timer.__green
             print(f"{color}Executed in {elapsed:.4f}s{Timer.__clear}")
             return result
@@ -20,12 +20,12 @@ class Timer:
         return wrapper
 
     def __enter__(self):
-        self.time = perf_counter()
+        self.time = perf_counter_ns()
         return self
 
     def __exit__(self, type, value, traceback):
-        self.time = perf_counter() - self.time
+        self.time = perf_counter_ns() - self.time
 
     def __str__(self):
-        color = Timer.__red if self.time > 10 else Timer.__green
-        return f"{color}{self.time:.4f}s{Timer.__clear}"
+        color = Timer.__red if self.time > 10e9 else Timer.__green
+        return f"{color}{self.time/1e9:.6f}s{Timer.__clear}"
