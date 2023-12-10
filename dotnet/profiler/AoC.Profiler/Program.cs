b352076fb451fe69d522f772998b967f83ce103c
Samuel Deslandes <sadeslandes@gmail.com>
2023-12-09 23:01:07 -0500

    Add profiler

---
+++ dotnet/profiler/AoC.Profiler/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.CommandLine;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using aoc.EntryPoint;
+using mazharenko.AoCAgent;
+using mazharenko.AoCAgent.Base;
+
+namespace AoC.Profiler;
+
+
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        var rootCmd = new RootCommand("aoc-profiler");
+        var repeatOption = new Option<int>(
+            aliases: ["--repeat", "-n"],
+            description: "Number of times to repeat execution",
+            getDefaultValue: () => 100);
+        var yearOption = new Option<int>(
+            aliases: ["--year", "-y"],
+            description: "Year of solution to profile",
+            getDefaultValue: () => DateTime.Now.Year);
+        var dayOption = new Option<string>(
+            aliases: ["--day", "-d"],
+            description: "Day of solution to profile");
+        var partOption = new Option<string>(
+            aliases: ["--part", "-p"],
+            description: "Number of times to repeat execution");
+
+        rootCmd.AddOption(repeatOption);
+        rootCmd.AddOption(yearOption);
+        rootCmd.AddOption(dayOption);
+        rootCmd.AddOption(partOption);
+        rootCmd.SetHandler(
+            (numRepeats, year, day, part) =>
+            {
+                Console.WriteLine($"Profiling year {year} day(s): {day} part(s): {part}. Repeat: {numRepeats}");
+                YearBase yearRunner;
+                Stopwatch stopwatch = new();
+                List<long> timings = new(capacity: numRepeats);
+
+                switch (year)
+                {
+                    case 2023:
+                        yearRunner = new Year2023();
+                        break;
+                    default:
+                        throw new InvalidOperationException($"Invalid year {year} specified");
+                }
+
+                foreach (RunnerDay rd in yearRunner.Days)
+                {
+                    if (day != "all" && rd.Num != int.Parse(day))
+                    {
+                        continue;
+                    }
+
+                    string dayInput = File.ReadAllText($"inputs/input{rd.Num:00}.txt");
+                    foreach (RunnerPart rp in new[] {rd.Part1, rd.Part2})
+                    {
+                        if (part != "all" && rp.Num != int.Parse(part))
+                        {
+                            continue;
+                        }
+
+                        for (int i = 0; i < numRepeats; i++)
+                        {
+                            try
+                            {
+                                stopwatch.Start();
+                                rp.Part.SolveObtained(dayInput);
+                                stopwatch.Stop();
+                            }
+                            catch (NotImplementedException)
+                            {
+                                return;
+                            }
+                            finally
+                            {
+                                stopwatch.Stop();
+                            }
+                            timings.Add(stopwatch.ElapsedTicks);
+                            stopwatch.Reset();
+                        }
+                        var duration = TimeSpan.FromTicks((long)timings.Average());
+                        if (duration.Seconds < 10)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        }
+                        Console.WriteLine($"Year {year}, Day {rd.Num}, Part {rp.Num}, {numRepeats} trials: {duration.TotalMilliseconds:.000}ms");
+                        timings.Clear();
+                        Console.ResetColor();
+                    }
+                }
+            },
+            repeatOption,
+            yearOption,
+            dayOption,
+            partOption);
+        return await rootCmd.InvokeAsync(args);
+    }
+}
--- dotnet/tests/AoC.Tests/InputTests.cs
+++ dotnet/tests/AoC.Tests/InputTests.cs
@@ -12,5 +12,13 @@ internal partial class InputTests
         yield return new PartInputCaseData(1, 2, "54265");
         yield return new PartInputCaseData(2, 1, "2486");
         yield return new PartInputCaseData(2, 2, "87984");
+        yield return new PartInputCaseData(3, 1, "528819");
+        yield return new PartInputCaseData(3, 2, "80403602");
+        yield return new PartInputCaseData(4, 1, "24848");
+        yield return new PartInputCaseData(4, 2, "7258152");
+        yield return new PartInputCaseData(5, 1, "");
+        yield return new PartInputCaseData(5, 2, "");
+        yield return new PartInputCaseData(6, 1, "");
+        yield return new PartInputCaseData(6, 2, "");
     }
 }
